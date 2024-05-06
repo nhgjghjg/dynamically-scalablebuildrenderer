c0f1a56a938931320bccc4f2bc64833f8fdefe47
nhgjghjg <esjcetusairanen@hotmail.com>
2024-05-06 17:01:22 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
